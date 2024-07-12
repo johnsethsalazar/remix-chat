7062147bc0c72fe2ddfc19b2a1c06ace757f8976
johnsethsalazar <salazarjohnseth@gmail.com>
2024-07-12 11:01:27 +0800

    Added Page, Layout, Card from Polaris. Output: https://monosnap.com/direct/mWty3OgFThwtibjlmNfw8hcxiAf1l9

--- app/routes/app.collections.tsx
+++ app/routes/app.collections.tsx
@@ -1,8 +1,15 @@
+import { Card, Layout, Page } from '@shopify/polaris'
 import React from 'react'
 
 const Collections = () => {
   return (
-    <div>Collections</div>
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card><h1>Hello World!</h1></Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
   )
 }
 
