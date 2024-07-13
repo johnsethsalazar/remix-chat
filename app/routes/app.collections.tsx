92802332c896d1a6e435a03db5ab06c58a5ebecc
johnsethsalazar <salazarjohnseth@gmail.com>
2024-07-13 09:49:48 +0800

    added function to Query collections from admin using loader function

--- app/routes/app.collections.tsx
+++ app/routes/app.collections.tsx
@@ -1,16 +1,73 @@
-import { Card, Layout, Page } from '@shopify/polaris'
-import React from 'react'
+import { LoaderFunction } from "@remix-run/node";
+import { Card, Layout, Page } from "@shopify/polaris";
+import { apiVersion, authenticate } from "~/shopify.server";
+
+// Injecting Method query for the try body
+export const query = `
+{
+    collections(first: 10){
+        edges{
+            node{
+                id
+                handle
+                title
+                description
+            }
+        }
+        pageInfo {
+            hasNextPage
+        }
+    }
+}
+`;
+
+// Query collections from admin using loader function
+export const loader: LoaderFunction = async ({ request }) => {
+  const { session } = await authenticate.admin(request);
+  const { shop, accessToken } = session;
+
+  try {
+    const response = await fetch(
+      `https://${shop}/admin/api/${apiVersion}/graphql.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/graphql",
+          "X-Shopify-Access-Token": accessToken!,
+        },
+        body: query,
+      },
+    );
+
+    //Handling response
+    if (response.ok) {
+      const data = await response.json();
+
+      const {
+        data: {
+          collections: { edges },
+        },
+      } = data;
+      return edges;
+    }
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Collections = () => {
   return (
     <Page>
       <Layout>
         <Layout.Section>
-          <Card><h1>Hello World!</h1></Card>
+          <Card>
+            <h1>Hello World!</h1>
+          </Card>
         </Layout.Section>
       </Layout>
     </Page>
-  )
-}
+  );
+};
 
-export default Collections
+export default Collections;
