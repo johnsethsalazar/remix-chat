43ed184b3897aa70e6af8f3e50166607ffbc2ed7
johnsethsalazar <salazarjohnseth@gmail.com>
2024-07-16 08:49:15 +0800

    Working with admin API. Query RestAPI. Output: https://monosnap.com/direct/0uXGiuVApBfL3BwwxA9ohGsEHoILHO

--- app/routes/app.inventory.tsx
+++ app/routes/app.inventory.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+import { authenticate } from "~/shopify.server";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const { admin, session } = await authenticate.admin(request);
+
+  try {
+    const response = await admin.rest.resources.InventoryLevel.all({
+      session: session,
+      location_ids: "99253584193",
+    });
+
+    if (response) {
+      console.log("hit", response);
+      const data = response.data;
+
+      console.log("data", data);
+
+      return json({
+        inventory: data,
+      });
+    }
+
+    return null;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const Inventory = () => {
-  return (
-    <div>app.inventory</div>
-  )
-}
+  const data: any = useActionData();
+  console.log(data, "data");
 
-export default Inventory
+  return <div>app.inventory</div>;
+};
+
+export default Inventory;
