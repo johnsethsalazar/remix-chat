fa366be0c4f4a1b43daa5147cd6719851a308f71
johnsethsalazar <salazarjohnseth@gmail.com>
2024-08-02 09:26:14 +0800

    had a cors problem when getting messages in the app.chat.tsx this way const response = await axios.get<Message[]>('https://remix-chat-backend-3h7y.vercel.app/api/getAllMessages') and temporarily solved it this way const response = await axios.get<Message[]>('https://cors-anywhere.herokuapp.com/https://remix-chat-backend-3h7y.vercel.app/api/getAllMessages') searched: https://www.google.com/search?q=cors-anywhere&rlz=1C1GCEA_enPH1087PH1087&oq=cors-anywhere&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGDwyBggCEEUYPDIGCAMQRRg80gEHMjIzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8 https://nordicapis.com/10-free-to-use-cors-proxies/ currently finding a more stable solution.

--- app/routes/app.chat.tsx
+++ app/routes/app.chat.tsx
@@ -1,7 +1,29 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+interface Message {
+  _id: string,
+  content: string
+}
+
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
+  const [messages, setMessages] = useState<Message[]>([])
+  // Function that will trigger everytime the page is reloaded
+  const getAllMessages = async () => {
+    try{
+      const response = await axios.get<Message[]>('https://cors-anywhere.herokuapp.com/https://remix-chat-backend-3h7y.vercel.app/api/getAllMessages')
+      setMessages(response.data)
+      console.log(response.data)
+    }catch(err){
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    getAllMessages()
+  }, [])
+
+  console.log('messages', messages)
+  return <div>Chat</div>
 }
 
 export default Chat
